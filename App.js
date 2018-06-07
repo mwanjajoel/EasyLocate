be5ae5d74d620e88399e2dbbe6ec8770693f3b1f
mwanjajoel <codebender@Codebenders-MacBook-Air.local>
2018-06-07 11:30:55 +0300

    Added capability to show location

--- App.js
+++ App.js
@@ -1,23 +1,108 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {Component} from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import {Constants, Location, Permissions } from 'expo';
+ 
 export default class App extends React.Component {
+
+  //state the initial state of the location 
+  state = {
+
+    location: null, 
+    errorMessage: null,
+  };
+
+  //mounting the component before running 
+  componentWillMount() {
+
+    //check if platform is android 
+    if (Platform.OS == "android" && !Constants.isDevice ) {
+
+      //set the state accordingly
+      this.setState({
+        errorMessage: "Dang! This cant work on an Android Emulator, try again on a real device!"
+      });
+
+    } else {
+
+      //run the location function
+      this._getLocationAsync();
+
+      }
+
+
+  }
+
+    //get the current location asyncronously
+    _getLocationAsync = async() => {
+
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+      //if status has not been granted 
+
+      if (status !== 'granted' ){
+
+        //set the state 
+        this.setState({
+          errorMessage: 'Permission to access location was denied! Sorry'
+        });
+
+      } else {
+
+        //however, if its granted, get the location
+        let location = await Location.getCurrentPositionAsync({});
+
+        //then set the state 
+        this.setState({
+          location
+        });
+
+      }
+
+
+    };
+
+  //then lets show the user 
   render() {
+
+    //set the text as we wait 
+    let text = "Waiting...";
+
+    //check if there is an error and report 
+    if (this.state.errorMessage) {
+
+      text = this.state.errorMessage;
+
+    } else if (this.state.location) {
+
+      text = JSON.stringify(this.state.location);
+
+    }
+
+    //and we show the user 
     return (
+
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.paragraph}>{text} </Text>
       </View>
+
     );
+
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1'
+  }, 
+    paragraph: {
+      margin: 24, 
+      fontSize: 18, 
+      textAlign: 'center',
+    },
+
 });
+
